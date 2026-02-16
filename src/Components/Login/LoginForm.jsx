810e5cb74f2260f100b30393a4b66f20f3b5c8bf
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-02-16 13:16:37 -0300

    Aula Form Component implementada

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { resumeAndPrerenderToNodeStream } from 'react-dom/static';
 import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginForm = () => {
   const [username, setUsername] = React.useState('');
@@ -28,9 +30,9 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
-        <input type="text" value={password} onChange={({ target }) => setPassword(target.value)} />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
