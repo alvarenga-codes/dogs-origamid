778eaee20f70a985a9b806086378a0a3fa72cbea
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-02-15 22:02:36 -0300

    Estrutura inicial implementada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+import Login from './Components/Login/Login';
+import './App.css';
 
 const App = () => {
-  return <div>App react</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
