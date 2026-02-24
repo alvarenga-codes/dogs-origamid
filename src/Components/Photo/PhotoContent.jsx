14b6ded59ccebe805f7e8b7c56e6519d2ffdf278
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-02-24 09:59:25 -0300

    Aula Foto delete implementada

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
 import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({ data }) => {
+  //Identificar o usuário
+  const user = React.useContext(UserContext);
+
   const { photo, comments } = data;
 
   return (
@@ -13,7 +18,12 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <p className={styles.author}>
-          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          {user.data && user.data.username === photo.author ? (
+            <PhotoDelete id={photo.id} />
+          ) : (
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          )}
+
           <span className={styles.visualizacoes}>{photo.acessos}</span>
         </p>
         <h1 className="title">
