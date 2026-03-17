dc9e80fe85377a95089f0a4ba4eb20a71f3c502f
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2026-03-17 19:59:58 -0300

    aula foto pagina implementada

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -6,14 +6,14 @@ import { UserContext } from '../../UserContext';
 import PhotoDelete from './PhotoDelete';
 import Image from '../Helper/Image';
 
-const PhotoContent = ({ data }) => {
+const PhotoContent = ({ single, data }) => {
   //Identificar o usuário
   const user = React.useContext(UserContext);
 
   const { photo, comments } = data;
 
   return (
-    <div className={styles.photo}>
+    <div className={`${styles.photo} ${single ? styles.single : ''}`}>
       <div className={styles.img}>
         <Image src={photo.src} alt={photo.title} />
       </div>
@@ -35,7 +35,7 @@ const PhotoContent = ({ data }) => {
           <li>{photo.idade} anos</li>
         </ul>
       </div>
-      <PhotoComments id={photo.id} comments={comments} />
+      <PhotoComments single={single} id={photo.id} comments={comments} />
     </div>
   );
 };
